4fee46c8d3e38209e02509d529d8809fc901d03b
kan3103 <khang310304@gmail.com>
2025-07-03 11:52:24 +0700

    Update benchmark bypassing attach

--- script/benchmark_bypassing_attach.rb
+++ script/benchmark_bypassing_attach.rb
@@ -8,37 +8,51 @@ dummy_path = Rails.root.join('tmp', 'images')
 def upload_zip(post, dummy_path)
     blobs = []
     time = Benchmark.realtime do
-      blobs = upload_all_files(dummy_path)
-      attachments = blobs.map do |blob|
-      {
-        name: "images",
-        record_type: "Post",
-        record_id: post.id,
-        blob_id: blob.id,
-        created_at: Time.current
-      }
-      end
-      ActiveStorage::Attachment.insert_all!(attachments)
+      upload_all_files(post.id, dummy_path)
     end
     time
 end
 
-def upload_all_files(extracted_folder)
+def insert_attachments(post_id, blobs)
+    now = Time.current
+    attachments = blobs.map do |blob|
+      {
+        name: "images",
+        record_type: "Post",
+        record_id: post_id,
+        blob_id: blob,
+        created_at: now
+      }
+    end
+    ActiveStorage::Attachment.insert_all!(attachments)
+end
+
+def upload_all_files(id, extracted_folder, batch_size: 400)
     blobs = []
-    Dir.glob(File.join(extracted_folder, '**', '*')) do |file_path|
+    Dir.glob(File.join(extracted_folder, '**', '*')).each_with_index do |file_path, idx|
       next if File.directory?(file_path)
 
       filename = File.basename(file_path)
 
       file = File.open(file_path)
-      blob = ActiveStorage::Blob.create_and_upload!(
-        io: file,
+      blobs << {
+        byte_size: file.size,
+        checksum: '',
         filename: filename,
-        content_type: Marcel::MimeType.for(file)
-      )
-      blobs << blob
+        metadata: {},
+        key: "test_uploads/#{id}/#{idx}/#{filename}",
+        service_name: 'localstack',
+        created_at: Time.current,
+      }
+      if blobs.size >= batch_size
+        ActiveStorage::Blob.insert_all!(blobs)
+        keys = blobs.map { |b| b[:key] }
+        inserted_blobs = ActiveStorage::Blob.where(key: keys).order(:id)
+        ids = inserted_blobs.pluck(:id)
+        insert_attachments(id, ids)
+        blobs.clear
+      end
     end
-    blobs
 end
 
 
@@ -47,15 +61,19 @@ end
 def db_insert(dummy_path, num: 10)
     puts "Inserting #{num} records with images without reload"
     times = 0
+    max, min = 0, Float::INFINITY
     num.times do |i|
         post = Post.create!(title: "Batch #{i + 1}", content: "Attaching images in batches")
-        puts "Uploading zip for batch #{i + 1}"
         time = upload_zip(post, dummy_path)
+        max = time if time > max
+        min = time if time < min
         times += time
-        puts "Time taken to upload files: #{time.round(2)} seconds"
-        puts "Batch #{i + 1} completed."
     end
-    puts "Total time for all batches: #{times.round(2)} seconds"
+    puts "Average time per record: #{(times / num).round(2)} seconds"
+    puts "Max time for a record: #{max.round(2)} seconds"
+    puts "Min time for a record: #{min.round(2)} seconds"
+    puts "Avg diff between max and min: #{(max - min).round(2)} seconds"
+    puts "Total time for all records: #{times.round(2)} seconds"
 end
 
 
@@ -86,5 +104,5 @@ def attach_in_batches(dummy_path, num: 2, batch_size: 400)
     puts "Sum of all batches: #{times.round(2)}s"
 end
 
-attach_in_batches(dummy_path, num: 3, batch_size: 400)
-db_insert(dummy_path, num: 3)
+attach_in_batches(dummy_path, num: 10, batch_size: 400)
+db_insert(dummy_path, num: 10)
